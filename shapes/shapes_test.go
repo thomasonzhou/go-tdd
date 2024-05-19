77de57627deb32b5e7e7f79a55a503158d0f5339
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 11:51:35 +0200

    Switch to table test

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -13,19 +13,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		assertFloat64Equal(t, got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 9.0}, 108.0},
+		{Circle{10}, 314.1592653589793},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rect := Rectangle{12.0, 9.0}
-		checkArea(t, rect, 108.0)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		assertFloat64Equal(t, got, tt.want)
+	}
+
 }
 
 func assertFloat64Equal(t *testing.T, f1, f2 float64) {
