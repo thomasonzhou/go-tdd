c01c8932fac93e5050820f38bc738fe8f7e74fe0
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 16:22:16 +0200

    Add server example

--- di/di.go
+++ di/di.go
@@ -1,10 +1,22 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, s string) {
+func Greet(writer io.Writer, s string) {
 	fmt.Fprintf(writer, "Hello, %s", s)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Jiminy Cricket")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
