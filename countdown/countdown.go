c4ff2db38aa0fc413cbe3858f6d9d431c8bac5f1
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 16:44:31 +0200

    Print counter at once

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
