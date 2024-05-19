f354027dfa7d57d01a6bb1be77ebc25e7ceed01d
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 14:57:21 +0200

    Pass tests for dictionary with error

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -4,10 +4,21 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dict := Dictionary{"grit": "doing hard stuff for a bit"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dict.Search("grit")
+		want := "doing hard stuff for a bit"
+		assertStringsEqual(got, want, t)
+	})
 
-	got := dict.Search("grit")
-	want := "doing hard stuff for a bit"
-	assertStringsEqual(got, want, t)
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dict.Search("unknown")
+		want := "could not find the word"
+
+		if err == nil {
+			t.Fatal("expected an error but didn't get one")
+		}
+		assertStringsEqual(want, err.Error(), t)
+	})
 }
 
 func assertStringsEqual(s1, s2 string, t *testing.T) {
