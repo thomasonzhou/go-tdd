f5f3a97b6eac253f74926d0cb70db306ef2b81aa
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 10:24:01 +0200

    Pass repeat test

---
+++ iteration/repeat_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestIteration(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
