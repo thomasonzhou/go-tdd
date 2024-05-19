9c38e25764fdf22d051f869e4ab517eb48adcd31
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 17:17:42 +0200

    Enable configurable sleep time

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -7,6 +7,23 @@ import (
 	"time"
 )
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 type Sleeper interface {
 	Sleep()
 }
--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -37,4 +38,16 @@ Go!`
 			t.Errorf("incorrect order of calls to sleeper, want %v got %v", want, spySleepPrinter.Calls)
 		}
 	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 22 * time.Second
+	spyTime := &SpyTime{}
+	configSleeper := &ConfigurableSleeper{sleepTime, spyTime.Sleep}
+
+	configSleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
