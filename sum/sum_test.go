48aaa3d9deec1a7e64e1dd9f99c571a7b02e5598
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 10:46:34 +0200

    Pass sum slice with 100% coverage

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,13 +3,13 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d wand %d given, %v", got, want, numbers)
-	}
+	t.Run("collection of variable size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
