9226db27e6aedb8eaf8762fb20ce124bcd0e15a6
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 10:27:21 +0200

    Specify repeat counts

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(s string) string {
+func Repeat(s string, count int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < count; i++ {
 		repeated += s
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,11 +2,17 @@ package iteration
 
 import "testing"
 
-func TestIteration(t *testing.T) {
-	repeated := Repeat("a")
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
 }
