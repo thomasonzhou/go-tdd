3703f04a00d22fb8a389bf9ab5715d4f6b26e809
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 10:29:28 +0200

    Add example

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 5)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("アニメ", 3)
+	fmt.Println(repeat)
+	// output: アニメアニメアニメ
 }
