84b296b62b19e8d5c41a218d48a211e93ce2f825
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 11:27:36 +0200

    Pass TestArea

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,5 +1,11 @@
 package shapes
 
-func Perimeter(width, height float64) (perimeter float64) {
-	return (width * 2) + (height * 2)
+// compute the perimeter of a rectangle
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+// compute the area of a rectangle
+func Area(width, height float64) float64 {
+	return width * height
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -6,7 +6,17 @@ func TestPerimeter(t *testing.T) {
 	got := Perimeter(10.0, 10.0)
 	want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	assertFloat64Equal(t, got, want)
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 9.0)
+	want := 108.0
+	assertFloat64Equal(t, got, want)
+}
+
+func assertFloat64Equal(t *testing.T, f1, f2 float64) {
+	if f1 != f2 {
+		t.Errorf("got %.2f want %.2f", f1, f2)
 	}
 }
