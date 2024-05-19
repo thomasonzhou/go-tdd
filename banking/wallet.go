c5b2bf3ea20c678508058fe3673d87f49bd21c54
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-19 12:50:49 +0200

    Add Bitcoin

--- banking/wallet.go
+++ banking/wallet.go
@@ -1,13 +1,15 @@
 package banking
 
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- banking/wallet_test.go
+++ banking/wallet_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	wallet := &Wallet{}
-	wallet.Deposit(42)
+	wallet.Deposit(Bitcoin(42))
 
 	got := wallet.Balance()
-	want := 42
+	want := Bitcoin(42)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
