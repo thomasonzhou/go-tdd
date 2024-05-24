0c3f9c6b93b7504b1b34ea3fec57e1c09885a56e
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-24 13:56:18 +0200

    Remove cancel to pass context through functions first

--- context/context_test.go
+++ context/context_test.go
@@ -2,31 +2,53 @@ package context
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+	// "time"
 )
 
 type SpyStore struct {
-	response  string
-	cancelled bool
-	t         *testing.T
+	response string
+	t        *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(111 * time.Millisecond)
-	return s.response
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, char := range s.response {
+			select {
+			case <-ctx.Done():
+				log.Println("spy store cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(char)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
+// func (s *SpyStore) Cancel() {
+// 	s.cancelled = true
+// }
 
 func TestServer(t *testing.T) {
 	t.Run("strings equal", func(t *testing.T) {
 		data := "Bonjour, 世界"
-		store := &SpyStore{data, false, t}
+		store := &SpyStore{response: data, t: t}
 		server := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -37,37 +59,37 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf("got %q wanted %q", response.Body.String(), data)
 		}
-		store.assertStoreWasCancelled()
+		// store.assertStoreWasCancelled()
 	})
 
-	t.Run("cancel if Cancel called", func(t *testing.T) {
-		data := "houston has a problem"
-		store := &SpyStore{response: data, t: t}
-		server := Server(store)
+	// t.Run("cancel if Cancel called", func(t *testing.T) {
+	// 	data := "houston has a problem"
+	// 	store := &SpyStore{response: data, t: t}
+	// 	server := Server(store)
 
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		cancellingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(7*time.Millisecond, cancel)
-		request = request.WithContext(cancellingCtx)
+	// 	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	// 	cancellingCtx, cancel := context.WithCancel(request.Context())
+	// 	time.AfterFunc(7*time.Millisecond, cancel)
+	// 	request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+	// 	response := httptest.NewRecorder()
 
-		server.ServeHTTP(response, request)
+	// 	server.ServeHTTP(response, request)
 
-		store.assertStoreCancelled()
-	})
+	// 	store.assertStoreCancelled()
+	// })
 }
 
-func (s *SpyStore) assertStoreCancelled() {
-	s.t.Helper()
-	if !s.cancelled {
-		s.t.Error("expected store to be cancelled")
-	}
-}
+// func (s *SpyStore) assertStoreCancelled() {
+// 	s.t.Helper()
+// 	if !s.cancelled {
+// 		s.t.Error("expected store to be cancelled")
+// 	}
+// }
 
-func (s *SpyStore) assertStoreWasCancelled() {
-	s.t.Helper()
-	if s.cancelled {
-		s.t.Error("store is not supposed to be cancelled")
-	}
-}
+// func (s *SpyStore) assertStoreWasCancelled() {
+// 	s.t.Helper()
+// 	if s.cancelled {
+// 		s.t.Error("store is not supposed to be cancelled")
+// 	}
+// }
