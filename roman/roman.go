40e8f1a772ad155a58602da481b326d91405120a
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-26 13:21:43 +0200

    Generalize switch using up to X

--- roman/roman.go
+++ roman/roman.go
@@ -7,6 +7,12 @@ func intToRoman(arabicNum int) string {
 	var result strings.Builder
 	for arabicNum > 0 {
 		switch {
+		case arabicNum > 9:
+			arabicNum -= 10
+			result.WriteString("X")
+		case arabicNum > 8:
+			arabicNum -= 9
+			result.WriteString("IX")
 		case arabicNum > 4:
 			arabicNum -= 5
 			result.WriteString("V")
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -21,6 +21,15 @@ func TestRomanConverter(t *testing.T) {
 			3, "III"},
 		{"4 to IV", 4, "IV"},
 		{"5 to V", 5, "V"},
+		{"6 to VI", 6, "VI"},
+		{"7 to VII", 7, "VII"},
+		{"8 to VIII", 8, "VIII"},
+		{"9 to IX", 9, "IX"},
+		{"10 to X", 10, "X"},
+		{"14 to XIV", 14, "XIV"},
+		{"18 to XVIII", 18, "XVIII"},
+		{"20 to XX", 20, "XX"},
+		{"39 to XXXIX", 39, "XXXIX"},
 	}
 
 	for _, test := range cases {
