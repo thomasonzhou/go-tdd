7fa67fa632ff0911dae9891c53e282a9696b4b93
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-05-26 13:31:14 +0200

    Generalize up to 1000

--- roman/roman_test.go
+++ roman/roman_test.go
@@ -30,6 +30,17 @@ func TestRomanConverter(t *testing.T) {
 		{"18 to XVIII", 18, "XVIII"},
 		{"20 to XX", 20, "XX"},
 		{"39 to XXXIX", 39, "XXXIX"},
+		{"40 to XL", 40, "XL"},
+		{"47 to XLVII", 47, "XLVII"},
+		{"49 to XLIX", 49, "XLIX"},
+		{"50 to L", 50, "L"},
+		{"90 to XC", 90, "XC"},
+		{"100 to C", 100, "C"},
+		{"400 to CD", 400, "CD"},
+		{"500 to D", 500, "D"},
+		{"900 to CM", 900, "CM"},
+		{"1000 to M", 1000, "M"},
+		{"3000 to MMM", 3000, "MMM"},
 	}
 
 	for _, test := range cases {
