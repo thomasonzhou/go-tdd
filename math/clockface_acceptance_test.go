fb860fb39e915ff53282b74cff2c2028cf78b078
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-06-09 17:39:47 +0200

    Cleanup xml

--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -11,27 +11,25 @@ import (
 
 type Svg struct {
 	XMLName xml.Name `xml:"svg"`
-	Text    string   `xml:",chardata"`
 	Xmlns   string   `xml:"xmlns,attr"`
 	Width   string   `xml:"width,attr"`
 	Height  string   `xml:"height,attr"`
 	ViewBox string   `xml:"viewBox,attr"`
 	Version string   `xml:"version,attr"`
-	Circle  struct {
-		Text  string `xml:",chardata"`
-		Cx    string `xml:"cx,attr"`
-		Cy    string `xml:"cy,attr"`
-		R     string `xml:"r,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"circle"`
-	Line []struct {
-		Text  string `xml:",chardata"`
-		X1    string `xml:"x1,attr"`
-		Y1    string `xml:"y1,attr"`
-		X2    string `xml:"x2,attr"`
-		Y2    string `xml:"y2,attr"`
-		Style string `xml:"style,attr"`
-	} `xml:"line"`
+	Circle  Circle   `xml:"circle"`
+	Line    Line     `xml:"line"`
+}
+type Circle struct {
+	Cx string `xml:"cx,attr"`
+	Cy string `xml:"cy,attr"`
+	R  string `xml:"r,attr"`
+}
+
+type Line []struct {
+	X1 string `xml:"x1,attr"`
+	Y1 string `xml:"y1,attr"`
+	X2 string `xml:"x2,attr"`
+	Y2 string `xml:"y2,attr"`
 }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
