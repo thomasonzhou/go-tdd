c68e85e1b9b133922059c2b8fb40e86602e147a5
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-06-09 17:36:14 +0200

    Pass acceptance test for xml management

--- math/clockface.go
+++ math/clockface.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -9,17 +11,37 @@ type Point struct {
 	X, Y float64
 }
 
-const SecondHandLength = 90
+const (
+	SecondHandLength = 90
+	ClockCenterX     = 150
+	ClockCenterY     = 150
+)
 
-const ClockCenterX = 150
-const ClockCenterY = 150
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
 
-func SecondHand(tm time.Time) Point {
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
+
+func secondHand(w io.Writer, tm time.Time) {
 	p := secondsToPoint(tm)
 	p = Point{p.X * SecondHandLength, p.Y * SecondHandLength} // Scale
 	p = Point{p.X, -p.Y}                                      // Flip vertically
 	p = Point{p.X + ClockCenterX, p.Y + ClockCenterY}         // Translate to origin
-	return p
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func secondsToRadians(tm time.Time) float64 {
--- math/clockface_acceptance_test.go
+++ math/clockface_acceptance_test.go
@@ -1,30 +1,56 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
 	"testing"
 	"time"
 
 	clockface "hello/math"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.October, 14, 0, 0, 0, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 - clockface.SecondHandLength}
-	got := clockface.SecondHand(tm)
-
-	if !clockface.RoughlyEqualPoint(want, got) {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.October, 14, 0, 0, 30, 0, time.UTC)
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(2024, time.October, 14, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 + clockface.SecondHandLength}
-	got := clockface.SecondHand(tm)
+	b := bytes.Buffer{}
+	clockface.SVGWriter(&b, tm)
 
-	if !clockface.RoughlyEqualPoint(want, got) {
-		t.Errorf("Got %v, wanted %v", got, want)
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150.000"
+	y2 := "60.000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
 	}
+
+	t.Errorf("Expected to find x2 %+v and y2 %+v in SVG %v", x2, y2, b.String())
 }
